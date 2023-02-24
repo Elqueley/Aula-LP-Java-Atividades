8063a99d53b56cba64e82572369c8e23a4deb9a3
ELQUELEY EVIN BARBOSA DA COSTA <evinelqueley@gmail.com>
2023-02-24 17:05:22 -0300

    primeira atividade

---
+++ src/revisao/atividade1.java
@@ -0,0 +1,17 @@
+package revisao;
+
+import java.util.Scanner;
+
+public class atividade1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Digite seu nome");
+		String nome = sc.nextLine();
+		
+		System.out.println("Ola :" + nome);
+
+	}
+
+}
