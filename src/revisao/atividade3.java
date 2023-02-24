a7e25416e9c23d43ddbe017c49108db495b67cde
ELQUELEY EVIN BARBOSA DA COSTA <evinelqueley@gmail.com>
2023-02-24 17:44:02 -0300

    terceira atividade

---
+++ src/revisao/atividade3.java
@@ -0,0 +1,22 @@
+package revisao;
+
+import java.util.Scanner;
+
+public class atividade3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Digite um numero inteiro");
+		int numero = sc.nextInt();
+		
+		System.out.println("Digite alguma coisa");
+		String frase = sc.nextLine();
+		
+		for(int i = 0; i < numero; i++) {
+			System.out.println(frase);
+		}
+
+	}
+
+}
